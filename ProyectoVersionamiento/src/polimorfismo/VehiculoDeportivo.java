ce793e35f9b63794c40a9f140a2f36712e16ea06
JORDY <JORDY@DESKTOP-C5P6L0M>
2021-02-27 00:13:25 -0500

    Versis 1.1 de la Clase VehiculoDeportivo - Ejercicio Polimorfismo

--- ProyectoVersionamiento/src/polimorfismo/VehiculoDeportivo.java
+++ ProyectoVersionamiento/src/polimorfismo/VehiculoDeportivo.java
@@ -1,5 +1,22 @@
 package polimorfismo;
 
 public class VehiculoDeportivo {
+	private int cilindrada;
 
+	public VehiculoDeportivo(int cilindrada, String matricula, String marca, String modelo) {
+		super(matricula, marca, modelo);
+		this.cilindrada = cilindrada;
+	}
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+	
+	public String mostrarDatos() {
+		return "Matricula es: " + matricula + "\nMarca: " + marca + "\nModelo: " + modelo + "\nCilindrada: " + cilindrada;
+	}
 }
