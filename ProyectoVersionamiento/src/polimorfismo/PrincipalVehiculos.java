4095e2bf2f08898c4a8b923312d9a570c7b3f76d
JORDY <JORDY@DESKTOP-C5P6L0M>
2021-02-26 23:15:43 -0500

    Version Inicial de clase PrincipalVehiculos

---
+++ ProyectoVersionamiento/src/polimorfismo/PrincipalVehiculos.java
@@ -0,0 +1,25 @@
+package polimorfismo;
+
+import mundo.Vehiculo;
+import mundo.VehiculoDeportivo;
+import mundo.VehiculoFurgoneta;
+
+public class PrincipalVehiculos {
+	public static void main(String[] args) {
+		System.out.println("VEHICULOS");	
+		System.out.println("-----------------------------");	
+		Vehiculo autos [] = new Vehiculo [3];
+		
+		autos [0] = new Vehiculo ("POI-2095", "Ford", "Explorer");
+		autos [1] = new VehiculoDeportivo (2000, "VIS-6490", "Mazda", "Mazda 3");
+		autos [2] = new VehiculoFurgoneta (4, "SOI-3905", "KIA", "Pregio");
+		
+
+		for(Vehiculo vehiculos: autos) {
+			System.out.println(vehiculos.mostrarDatos());
+			//System.out.println("");	
+			System.out.println("-----------------------------");	
+		}
+	}
+
+}
